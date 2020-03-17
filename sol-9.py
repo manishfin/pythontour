39aef024781e306be249b03fd6aa5135e4b79b89
Manish Sharma <manishsharma@Manishs-MacBook-Air.local>
2020-03-17 20:41:54 +0530

    Add solution of exercise 9

---
+++ sol-9.py
@@ -0,0 +1,14 @@
+# Ex-9
+# Mutable vs Immutable data types
+# Follow this video https://www.youtube.com/watch?v=5qQQ3yzbKp8
+# Label the following datatypes whether they are mutable or immutable
+# String
+# List
+# Dictionary
+# Tuple
+
+# sol:
+# String - Immutable
+# List - Mutable
+# Dictionary - Mutable
+# Tuple - Immutable
