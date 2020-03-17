dccb46d67b7789b8cf538fb268439c56e7981d5d
Manish Sharma <manishsharma@Manishs-MacBook-Air.local>
2020-03-17 20:41:07 +0530

    Add solution of exercise 5

---
+++ sol-5.py
@@ -0,0 +1,17 @@
+# Ex-5
+# Read about the module random, it has method randint().
+# https://docs.python.org/2/library/random.html
+# Create a list with 100 random integers.
+
+# sol:
+from random import randint
+
+def getRandomIntegers(length, start=0, stop=100):
+    integersList = []
+    while length > 0:
+        integersList.append(randint(start, stop))
+        length -= 1
+    return integersList
+
+if __name__ == "__main__":
+    print(getRandomIntegers(100))
