afd83e5683ead14a1d48d3cbf849207a38db67df
Manish Sharma <manishsharma@Manishs-MacBook-Air.local>
2020-03-17 16:00:35 +0530

    Add solution of exercise 1

---
+++ sol-1.py
@@ -0,0 +1,18 @@
+ # Ex-1 Spot the error
+'''
+if 2>3:
+puts "2 is greater than 3"
+else:
+puts "2 is not greater than 3"
+'''
+
+# sol:
+
+if 2 > 3:
+    print ("2 is greater than 3")
+else:
+    print ("2 is not greater than 3")
+
+# Exp:
+# 1. Indentation errors at line number 4 & 6.
+# 2. There is no 'puts' keyword in python so instead of 'puts' we should use 'print' keyword.
