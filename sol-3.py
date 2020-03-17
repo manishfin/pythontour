765bd5dc83369a53775d316d9d0e081b3d0b7d2b
Manish Sharma <manishsharma@Manishs-MacBook-Air.local>
2020-03-17 17:07:29 +0530

    Add solution of exercise 3

---
+++ sol-3.py
@@ -0,0 +1,12 @@
+# Ex-3
+# Write a function print_twice.
+# Take a number as input from user and print twice the number
+# eg: print_twice(2)
+# Output: 4
+
+# sol:
+def print_twice(number):
+    print (2 * number)
+
+if __name__ == "__main__":
+    print_twice(2)
