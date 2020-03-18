ddd1dc0c62c5f371550decefa02dfdfa01f4377a
Manish Sharma <manishsharma@Manishs-MacBook-Air.local>
2020-03-18 18:54:25 +0530

    modified

--- sol-4.py
+++ sol-4.py
@@ -4,12 +4,7 @@
 
 # sol:
 def getFileNames(fileNames, extention):
-    outputFileNames = []
-    for fileName in fileNames:
-        fileExt = fileName.split(".")[-1]
-        if fileExt == extention:
-            outputFileNames.append(fileName)
-    return outputFileNames
+    return [fileName for fileName in fileNames if fileName.endswith(extention)]
 
 if __name__ == "__main__":
     filenames = ['file1.txt', 'file2.txt', 'file3.pdf', 'file4.pdf']
